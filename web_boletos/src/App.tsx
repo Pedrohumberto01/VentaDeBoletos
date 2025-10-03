e81675f4d744c8ac1a63270f8ac77f3dc9dd00d8
Jairo Joaquin <jairojoaquin25@gmail.com>
2025-10-02 21:30:07 -0600

    Agregando tailwind y MUI

--- web_boletos/src/App.tsx
+++ web_boletos/src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Login from './presentation/Login/Login';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Web de boletos</h1>} />
+      <Route path="/" element={<Login/>} />
     </Routes>
   );
 }
---
+++ web_boletos/src/presentation/Login/Login.tsx
@@ -0,0 +1,5 @@
+export default function Login() {
+  return (
+    <div className="text-4xl font-bold bg-amber-200">Dual</div>
+  )
+}
